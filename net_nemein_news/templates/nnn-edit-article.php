fedfda566df32832d3499a065981795d0212d3ee
Henri Bergius <henri.bergius@iki.fi>
2008-04-10 13:57:38 +0300

    Baseclass for object management via datamanager

--- net_nemein_news/templates/nnn-edit-article.php
+++ net_nemein_news/templates/nnn-edit-article.php
@@ -3,5 +3,5 @@
  * @package midcom_core
  */
 ?>
-<div tal:content="structure net_nemein_news/article_dm_form/as_html">
+<div tal:content="structure net_nemein_news/datamanager_form/as_html">
 </div>
