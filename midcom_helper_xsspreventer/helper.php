90c9b9be1323af31e8b3e868093aa8ea14c51f70
Henri Bergius <henri.bergius@iki.fi>
2008-04-10 16:35:30 +0300

    Static methods

--- midcom_helper_xsspreventer/helper.php
+++ midcom_helper_xsspreventer/helper.php
@@ -2,23 +2,23 @@
 
 class midcom_helper_xsspreventer_helper
 {
-    public function value($input)
+    static public function value($input)
     {
         $output = str_replace('"', '&quot;', $input);
         return '"' . $output . '"';
     }
 
-    public function textarea($input)
+    static public function textarea($input)
     {
-        return $this->escape_tag_close('textarea', $input);
+        return midcom_helper_xsspreventer_helper::escape_tag_close('textarea', $input);
     }
 
-    public function option($input)
+    static public function option($input)
     {
-        return $this->escape_tag_close('option', $input);
+        return midcom_helper_xsspreventer_helper::escape_tag_close('option', $input);
     }
 
-    public function escape_tag_close($tagname, $input)
+    static public function escape_tag_close($tagname, $input)
     {
         return preg_replace_callback
         (
